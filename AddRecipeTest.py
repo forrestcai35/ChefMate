2fc3a1e72179af6aa57a38c0bcc026d004a2043c
Forrest Cai <forrestcai35@gmail.com>
2024-02-18 17:52:45 -0500

    edit

--- AddRecipeTest.py
+++
@@ -1,6 +0,0 @@
-import OpenAI
-
-ChefMate = OpenAI.ChefMate()
-
-ChefMate.ChefMateReply("Please give me a recipe for chicken.")
-print(ChefMate.ChefMateReply("Please add the recipe you just outputted in the format given."))
---
+++ tests/AddRecipeTest.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory to sys.path
+current_dir = Path(__file__).parent
+parent_dir = current_dir.parent
+sys.path.append(str(parent_dir))
+
+import OpenAI
+
+ChefMate = OpenAI.ChefMate()
+
+ChefMate.ChefMateReply("Please give me a recipe for chicken.")
+print(ChefMate.ChefMateReply("Please add the recipe you just outputted in the format given."))
